9a7f648740f57b86553ad0a517911194795738e9
Christoph Raitzig <christoph@quikk.de>
2023-11-15 19:39:39 +0100

    Allow configuration of API URLs via environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-enum Api {
-  USER_API = "http://localhost:3004",
-  FEED_API = "http://localhost:3001",
-  PRESCRIPTION_API = "http://localhost:3005",
+const Api = {
+  USER_API: process.env.EXTERNAL_USER_API_URL || "http://localhost:3004",
+  FEED_API: process.env.EXTERNAL_FEED_API_URL || "http://localhost:3001",
+  PRESCRIPTION_API: process.env.EXTERNAL_PRESCRIPTION_API_URL || "http://localhost:3005",
 }
 
 export default Api;
