58066bc7c32fcd93640a0309e72429fcd74cca67
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2024-01-22 16:12:50 +0100

    🍱 Update logo

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,33 +1,35 @@
 import * as React from "react";
-import { Stack, Typography, useTheme } from "@mui/material";
+import {Stack, Typography, useTheme} from "@mui/material";
 import Link from "@/components/common/Link";
 import Size from "@/components/layout/size";
 import logo from "@/assets/hemmo.svg";
-import { useSelector } from "react-redux";
-import { Store } from "@/redux";
-import { ADMIN_ROLE } from "@/auth/auth.constants";
+import {useSelector} from "react-redux";
+import {Store} from "@/redux";
+import {ADMIN_ROLE} from "@/auth/auth.constants";
 
 const Navbar: React.FC = () => {
-  const theme = useTheme();
+    const theme = useTheme();
 
-  const { roles } = useSelector((s: Store) => s.auth);
+    const {roles} = useSelector((s: Store) => s.auth);
 
-  return (
-    <Typography variant="h6">
-      <Stack direction="row" spacing={Size.LARGE} alignItems="center">
-        <Link href="/">
-          <img
-            src={logo}
-            // @ts-ignore
-            height={theme.spacing(3)}
-          />
-        </Link>
-        <Link href="/users">Nutzer</Link>
-        <Link href="/news">News</Link>
-        {roles.includes(ADMIN_ROLE) && <Link href="/admin">Admin</Link>}
-      </Stack>
-    </Typography>
-  );
+    return (
+        <Typography variant="h6">
+            <Stack direction="row" spacing={Size.LARGE} alignItems="center">
+                <Link href="/">
+                    <Stack justifyContent="center" alignItems="center">
+                        <img
+                            src={logo}
+                            // @ts-ignore
+                            height={theme.spacing(3)}
+                        />
+                    </Stack>
+                </Link>
+                <Link href="/users">Nutzer</Link>
+                <Link href="/news">News</Link>
+                {roles.includes(ADMIN_ROLE) && <Link href="/admin">Admin</Link>}
+            </Stack>
+        </Typography>
+    );
 };
 
 export default Navbar;
