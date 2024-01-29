c60439b326885ce227ec7fab3827d825a7b18e1e
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2024-01-29 11:19:58 +0100

    🩹 Only allow center and admin users to access the dashboard

---
+++ src/components/auth/auth.constants.ts
@@ -0,0 +1,5 @@
+export const VISITOR_ROLE = "visitor";
+export const PATIENT_ROLE = "patient";
+export const PROFESSIONAL_ROLE = "professional";
+export const CENTER_ROLE = "center";
+export const ADMIN_ROLE = "admin";
