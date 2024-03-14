5b41eb0c78a38254b8d64f41c977579eae2317d0
Joyce Marvin Rafflenbeul <joycerafflenbeul@googlemail.com>
2024-03-14 09:23:09 +0100

    🐛 Fix pagination issue in user table

--- src/components/overview/users/cells/MoreActionsButton.tsx
+++ src/components/overview/users/cells/MoreActionsButton.tsx
@@ -1,15 +1,8 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
-import { Divider, IconButton, Menu, MenuItem } from "@mui/material";
-import { MoreVert } from "@mui/icons-material";
-import useBlockUser from "@/api/users/useBlockUser";
-import useUnblockUser from "@/api/users/useUnblockUser";
-import useActivateUser from "@/api/users/useActivateUser";
-import useDeactivateUser from "@/api/users/useDeactivateUser";
+import React, { useCallback, useMemo } from "react";
+import { Divider } from "@mui/material";
 import logger from "@/core/logger";
-import { useSelector } from "react-redux";
-import { Store } from "@/redux";
 import useGetUsers from "@/api/users/useGetUsers";
-import { initialTableConfig } from "@/components/overview/table/useTableConfig";
+import useTableConfig from "@/components/overview/table/useTableConfig";
 import Verify from "@/components/overview/users/cells/Verify";
 import Block from "@/components/overview/users/cells/Block";
 import ViewPrescriptions from "@/components/overview/users/cells/ViewPrescriptions";
@@ -26,14 +19,9 @@ const MoreActionsButton: React.FC<Props> = ({ id, blocked, enabled }) => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const menuOpen = useMemo(() => Boolean(anchorEl), [anchorEl]);
 
-  const { request } = useGetUsers(initialTableConfig);
+  const { pageNumber, pageSize, query } = useTableConfig(true);
 
-  const handleClick = useCallback(
-    (event: React.MouseEvent<HTMLButtonElement>) => {
-      setAnchorEl(event.currentTarget);
-    },
-    [setAnchorEl],
-  );
+  const { request } = useGetUsers({ pageNumber, pageSize, query });
 
   const handleClose = useCallback(() => {
     setAnchorEl(null);
